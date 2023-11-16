b332bfed5013a8711c024a1ce4ddce14d6a6d51b
fireing123 <88523379+fireing123@users.noreply.github.com>
2023-11-16 11:18:58 +0900

    t

--- src/app/modify/[id]/page.tsx
+++ src/app/modify/[id]/page.tsx
@@ -2,18 +2,18 @@ import { MDXRemote } from "next-mdx-remote/rsc"
 
 export default async function Modify({ params } : { params: { id: string } }) {
 
-    /*const res = await fetch(`/api/post/${params.id}`)<div>{name}</div>
-            <div>{date}</div>
+    const res = await fetch(`/api/post/${params.id}`)
     const { type, name, date, authorId, content } = await res.json()
     if (!type) {
         return <div>공개되지않음</div>
     }
-    const mdRes = await fetch("")
-    const md = await mdRes.text()*/
+    const mdRes = await fetch(content)
+    const md = await mdRes.text()
     
     return (
         <div>
-            
+            <div>{name}</div>
+            <div>{date}</div>
             <MDXRemote source={md} />
         </div>
     )

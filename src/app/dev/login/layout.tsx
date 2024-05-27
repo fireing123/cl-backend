4139c2fde74bffe9aac15a6811a543839ab9dfdb
fireing123 <gimd82368@gmail.com>
2024-05-27 21:23:49 +0900

    suspspspspsp

--- src/app/dev/login/layout.tsx
+++ src/app/dev/login/layout.tsx
@@ -1,9 +1,4 @@
-import { Metadata } from "next";
 import { Suspense } from "react";
-
-export const metadata: Metadata = {
-    title: '로그인 페이지',
-}
 
 export default async function Layout({
     children,

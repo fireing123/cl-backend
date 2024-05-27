0493ec455dfbc3d1c0c72ab4cd4fa9cfdc1e4525
fireing123 <gimd82368@gmail.com>
2024-05-27 21:13:03 +0900

    email

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -4,6 +4,9 @@ const { EMAIL, EMAIL_PASS } = process.env;
 
 export const transporter = nodemailer.createTransport({
     service: "gmail",
+    port: 587,
+    secure: false,
+    host: 'smtp.gmail.com',
     auth: {
         user: EMAIL,
         pass: EMAIL_PASS
